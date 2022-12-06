dcbd4f13f7b0bc05cda38748ca4318dd91e73bbb
YASH SINGHAL <95312054+YashSinghal215@users.noreply.github.com>
2022-12-06 22:24:46 +0530

    Add files via upload

---
+++ Linear_search.cpp
@@ -0,0 +1,41 @@
+//Yash Singhal CST-SPL-2
+
+#include<iostream>
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    vector<int>v;
+    int target,n;
+
+    cout<<"Enter size and element in vector"<<endl;
+
+    cin>>n;
+    for(int i=0;i<n;i++)
+    {
+        int x;
+        cin>>x;
+        v.push_back(x);
+    }
+
+    cout<<"Enter Target"<<endl;
+    cin>>target;
+    int flag = 0;
+
+    for(int i=0;i<n;i++)
+    {
+        if(v[i] == target)
+        {
+            cout<<"Target found at: "<<i<<endl;
+            flag = 1;
+        }
+    }
+
+    if(flag == 0)
+    {
+        cout<<"Not Found"<<endl;
+    }
+
+}
