e95a87de0f8378df86c72071575c83dbf4de96a7
YASH SINGHAL <95312054+YashSinghal215@users.noreply.github.com>
2022-12-10 10:15:19 +0530

    Update 4_Iterative_Factorial.cpp

--- 4_Iterative_Factorial.cpp
+++ 4_Iterative_Factorial.cpp
@@ -1,4 +1,4 @@
-// Yash Singhal
+// Yash Singhal Iterative
 
 #include<iostream>
 #include<bits/stdc++.h>
@@ -14,6 +14,40 @@ int factorial(int n)
     }
     return x;
 }
+int main()
+{
+    int n;
+    cout<<"Enter the no."<<endl;
+
+    cin>>n;
+
+    int ans = factorial(n);
+
+    cout<<ans;
+
+    return 0;
+}
+
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    
+    //Yash Singhal CST-SPL-2  Recursion
+
+#include<iostream>
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int factorial(int n)
+{
+    if(n == 0)
+        return 1;
+
+    if(n == 1)
+        return 1;
+
+    return n*factorial(n-1);
+}
+
 int main()
 {
     int n;
