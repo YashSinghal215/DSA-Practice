e1b8cc6a1dc1b4d2eec4a1471fefdc2e7f35b853
YASH SINGHAL <95312054+YashSinghal215@users.noreply.github.com>
2022-12-13 22:31:13 +0530

    Add files via upload

---
+++ 8_Selection_sort.cpp
@@ -0,0 +1,45 @@
+// Yash Singhal CST SPL 2
+
+#include<iostream>
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    
+    int n;
+    cout<<"Enter no. of process"<<endl;
+    cin>>n;
+    vector<int>v(n);
+
+    cout<<"ENter elements"<<endl;
+    for(int i=0;i<n;i++)
+    {
+        cin>>v[i];
+    }
+
+    for(int i=0;i<n-1;i++)
+    {
+        int p = i;
+
+        for(int j=i+1;j<n;j++)
+        {
+            if(v[j] < v[p])
+            {
+                p = j;
+            }
+        }
+
+        swap(v[i],v[p]);
+    }
+
+    cout<<"After sort"<<endl;
+
+    for(int i=0;i<n;i++)
+    {
+        cout<<v[i]<<" ";
+
+    }
+
+}
